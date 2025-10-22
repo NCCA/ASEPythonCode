6e6b9f448a8cfca7b8e1f8b3a39f183db94be026
Jon Macey <jmacey@bournemouth.ac.uk>
2025-10-22 10:48:08 +0100

    basic working Particle system

--- Lab6/Particle/Vec3.py
+++ Lab6/Particle/Vec3.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Vec3:
     def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
         self._x = x
@@ -9,6 +12,17 @@ class Vec3:
 
     def __repr__(self):
         return f"Vec3({self._x}, {self._y}, {self._z})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Vec3):
+            return NotImplemented
+        return math.isclose(self._x, other._x) and math.isclose(self._y, other._y) and math.isclose(self._z, other._z)
+
+    def to_tuple(self):
+        return (self._x, self._y, self._z)
+
+    def to_list(self):
+        return [self._x, self._y, self._z]
 
     @property
     def x(self):
@@ -39,3 +53,27 @@ class Vec3:
         if not isinstance(value, (int, float)):
             raise TypeError("x must be a number")
         self._z = float(value)
+
+    def __mul__(self, other):
+        if isinstance(other, Vec3):
+            return Vec3(self._x * other._x, self._y * other._y, self._z * other._z)
+        elif isinstance(other, (int, float)):
+            return Vec3(self._x * other, self._y * other, self._z * other)
+        else:
+            raise TypeError("unsupported operand type(s) for *: 'Vec3' and '{}'".format(type(other).__name__))
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+    def __iadd__(self, other):
+        if isinstance(other, Vec3):
+            self._x += other._x
+            self._y += other._y
+            self._z += other._z
+        elif isinstance(other, (int, float)):
+            self._x += other
+            self._y += other
+            self._z += other
+        else:
+            raise TypeError("unsupported operand type(s) for +=: 'Vec3' and '{}'".format(type(other).__name__))
+        return self
--- Lab6/Particle/main.py
+++ Lab6/Particle/main.py
@@ -1,5 +1,44 @@
+import matplotlib.pyplot as plt
+from Emitter import Emitter
+from mpl_toolkits.mplot3d import Axes3D
+from Vec3 import Vec3
+
+
+def plot_emitter(emitter):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+
+    for _ in range(20):  # run for 200 frames
+        emitter.update(0.5)
+        # extract positions and colours
+        positions = [p.position for p in emitter.particles]
+        colours = [p.colour.to_tuple() for p in emitter.particles]
+
+        # ax.clear()
+        if positions:
+            x_coords = [p.x for p in positions]
+            y_coords_sim = [p.y for p in positions]
+            z_coords_sim = [p.z for p in positions]
+            ax.scatter(x_coords, z_coords_sim, y_coords_sim, c=colours)
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Z")
+    ax.set_zlabel("Y")
+    ax.set_xlim([-20, 20])
+    ax.set_ylim([-20, 20])
+    ax.set_zlim([-20, 20])
+    plt.title("Particle System")
+
+    plt.show()
+
+
 def main():
-    print("Hello from particle!")
+    emitter = Emitter(Vec3(0, 0, 0), 50)
+    # plot_emitter(emitter)
+
+    for i in range(20):
+        emitter.write_geo(f"frame_{i}.geo")
+        emitter.update(0.5)
 
 
 if __name__ == "__main__":
